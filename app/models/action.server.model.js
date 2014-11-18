33f0819d15f070ece5e59cf146e42eccf8940104
MGrin <mr6r1n@gmail.com>
2014-11-18 13:48:37 +0100

    remove public/lib from repo, setup bower, change the calendar (needs to be changed on frontend)

---
+++ app/models/action.server.model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var app; // all application-wide things like ENV, config, logger, etc
+var mongoose = require('mongoose');
+var moment = require('moment');
+var Schema = mongoose.Schema;
+var _ = require('underscore');
+var async = require('async');
+var ObjectId = Schema.ObjectId;
+var troop = require('mongoose-troop');
+
+/**
+ * Models
+ */
+exports.initModel = function (myApp) {
+  app = myApp;
+};
+
+var ActionSchema = exports.Schema = new Schema({
+  userId: {
+    type: ObjectId,
+    ref: 'Users'
+  },
+  type: String
+});
+
+ActionSchema.plugin(troop.timestamp, {
+  useVirtual: false
+});
