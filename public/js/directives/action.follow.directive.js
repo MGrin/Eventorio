6435917bd05ce200d19c9b27359612a9ef1e4d50
MGrin <mr6r1n@gmail.com>
2014-12-15 14:53:08 +0100

    add links to images in news

--- public/js/directives/action.follow.directive.js
+++ public/js/directives/action.follow.directive.js
@@ -7,6 +7,7 @@ app.directive('followAction', ['Global', function (Global) {
     link: function ($scope, element, attrs) {
       $(element).find('#actionTimestamp').text(moment($scope.action.created).format('MMMM Do YYYY, HH:mm:ss'));
       $(element).find('#actionImg').attr('src', $scope.action.subject[0].userId.picture);
+      $(element).find('#actionImgLink').attr('href', '/users/'+$scope.action.subject[0].userId.username);
       $(element).find('#subjectLink').attr('href', '/users/' + $scope.action.subject[0].userId.username);
       $(element).find('#subjectName').text($scope.action.subject[0].userId.username + ' ');
       $(element).find('#objectLink').attr('href', '/users/' + $scope.action.object[0].userId.username);
@@ -15,6 +16,7 @@ app.directive('followAction', ['Global', function (Global) {
       $(element).find('#secondActionImg').css('margin-top', '10px');
       $(element).find('#secondActionImg').attr('src',
           $scope.action.object[0].userId.picture);
+      $(element).find('#secondActionImgLink').attr('href', '/users/'+$scope.action.object[0].userId.username);
     },
   }
 }]);
--- public/js/directives/action.signup.directive.js
+++ public/js/directives/action.signup.directive.js
@@ -6,6 +6,7 @@ app.directive('signupAction', ['Global', function (Global) {
     templateUrl: '/view/actions/signup.html',
     link: function ($scope, element, attrs) {
       $(element).find('#actionImg').attr('src', $scope.action.subject[0].userId.picture);
+      $(element).find('#actionImgLink').attr('href', '/users/' + $scope.action.subject[0].userId.username);
       $(element).find('#subjectLink').attr('href', '/users/' + $scope.action.subject[0].userId.username);
       $(element).find('#subjectLink').html('<b>' + $scope.action.subject[0].userId.username + '</b>');
       $(element).find('#actionTimestamp').text(moment($scope.action.created).format('MMMM Do YYYY, HH:mm:ss'));
