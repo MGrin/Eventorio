fea1fc97fcce685cd337912e90bda8bae34630bd
MGrin <mr6r1n@gmail.com>
2014-12-15 15:03:00 +0100

    change user name color on mobile screen

--- public/cdn/js/directives/action.invite.directive.js
+++ public/cdn/js/directives/action.invite.directive.js
@@ -7,6 +7,7 @@ app.directive('inviteAction', ['Global', function (Global) {
     link: function ($scope, element, attrs) {
       $(element).find('#actionTimestamp').text(moment($scope.action.created).format('MMMM Do YYYY, HH:mm:ss'));
       $(element).find('#actionImg').attr('src', $scope.action.object[0].userId.picture);
+      $(element).find('#actionImgLink').attr('href', '/users/'+$scope.action.object[0].userId.username);
       $(element).find('#subjectLink').attr('href', '/events/' + $scope.action.subject[0].eventId.id);
       $(element).find('#subjectName').text(' @' + $scope.action.subject[0].eventId.name);
       $(element).find('#objectLink').attr('href', '/users/' + $scope.action.object[0].userId.username);
@@ -15,6 +16,7 @@ app.directive('inviteAction', ['Global', function (Global) {
       $(element).find('#secondActionImg').css('margin-top', '10px');
       $(element).find('#secondActionImg').attr('src',
           $scope.action.subject[0].eventId.picture || '/img/event_logo.jpg');
+      $(element).find('#secondActionImgLink').attr('href', '/events/'+$scope.action.subject[0].eventId.id);
     },
   }
 }]);
--- public/cdn/js/directives/action.quitEvent.directive.js
+++ public/cdn/js/directives/action.quitEvent.directive.js
@@ -7,6 +7,7 @@ app.directive('quitEventAction', ['Global', function (Global) {
     link: function ($scope, element, attrs) {
       $(element).find('#actionTimestamp').text(moment($scope.action.created).format('MMMM Do YYYY, HH:mm:ss'));
       $(element).find('#actionImg').attr('src', $scope.action.subject[0].userId.picture);
+      $(element).find('#actionImgLink').attr('href', '/users/'+$scope.action.subject[0].userId.username);
       $(element).find('#subjectLink').attr('href', '/users/' + $scope.action.subject[0].userId.username);
       $(element).find('#subjectName').text($scope.action.subject[0].userId.username + ' ');
       $(element).find('#objectLink').attr('href', '/events/' + $scope.action.object[0].eventId.id);
@@ -15,6 +16,7 @@ app.directive('quitEventAction', ['Global', function (Global) {
       $(element).find('#secondActionImg').css('margin-top', '10px');
       $(element).find('#secondActionImg').attr('src',
           $scope.action.object[0].eventId.picture || '/img/event_logo.jpg');
+      $(element).find('#secondActionImgLink').attr('href', '/events/'+$scope.action.object[0].eventId.id)
     },
   }
 }]);
