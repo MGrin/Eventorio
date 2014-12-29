412636bf2f3168a844014f02349687a0f5570d4a
MGrin <mr6r1n@gmail.com>
2014-12-29 23:55:24 +0100

    implement persons references in comments

--- public/js/directives/comment.directive.js
+++ public/js/directives/comment.directive.js
@@ -8,7 +8,21 @@ app.directive('comment', ['Global', function (Global) {
       $(element).find('#userLink').attr('href', '/users/' + $scope.comment.creator.username);
       $(element).find('#userImg').attr('src', $scope.comment.creator.picture);
       $(element).find('#username').text($scope.comment.creator.username);
-      $(element).find('#content').html($scope.comment.content);
+
+      var contentSplit = $scope.comment.content.split(' ');
+      var resContent = [];
+      _.each(contentSplit, function (word) {
+        var temp = word.match(/@[a-zA-Z0-9]+/);
+        if (temp) {
+          var username = temp[0].substring(1);
+          word = word.substring(0, temp.index)
+                  + '<b><a href="/users/' + username + '">@' + username + '</a></b>'
+                  + word.substring(temp.index + username.length + 1);
+          console.log(word)
+        }
+        resContent.push(word);
+      });
+      $(element).find('#content').html(resContent.join(' '));
       $(element).find('#commentTimestamp').text(moment($scope.comment.created).format('MMMM Do YYYY, HH:mm:ss'));
     },
   }
