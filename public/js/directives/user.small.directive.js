d1d7a614e464b722cacf1af2c60e28e464703884
MGrin <mr6r1n@gmail.com>
2014-12-30 07:41:55 +0100

    small visual fixes

--- public/js/directives/user.small.directive.js
+++ public/js/directives/user.small.directive.js
@@ -3,12 +3,12 @@ app.directive('userSmall', ['Global', function (Global) {
     scope: {
       user: '=user'
     },
-    template: '<a><img class="img img-responsive img-thumbnail">' +
-              '<div class="text-center"></a>',
+    template: '<a><div class="row"><img class="col-lg-12 col-xs-12 img img-responsive img-thumbnail"></div>' +
+              '<div class="margin-lg-10"></div><div class="row"><label class="label label-default col-lg-10 col-lg-offset-1 col-xs-12 col-xs-offset-0"></label></div></a>',
     link: function ($scope, element, attrs) {
       $(element).find('a').attr('href', '/users/'+$scope.user.username);
       $(element).find('img').attr('src', $scope.user.picture);
-      $(element).find('div').text($scope.user.username);
+      $(element).find('label').text($scope.user.username);
     },
   }
 }]);
