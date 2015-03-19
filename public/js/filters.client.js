b9a3e0ea45884a1e8668c73c7e45a12de35df015
MGrin <mr6r1n@gmail.com>
2015-03-19 15:14:25 +0100

    fix event-line shown description if there is no description

--- public/js/filters.client.js
+++ public/js/filters.client.js
@@ -1,5 +1,6 @@
 app.filter('plainText', function () {
   return function (text) {
+    if (!text || text === 'undefined') return '';
     var res = String(text).replace(/<[^>]+>/gm, ' ');
     if (res.length > 30) res = res.substring(0, 30) + '...';
     return res;
