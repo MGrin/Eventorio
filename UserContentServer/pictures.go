5841adab5e920241db9d6225316d75182d9bbe20
mgrin <mr6r1n@gmail.com>
2015-06-23 17:41:26 +0200

    fix constants in go server

--- UserContentServer/pictures.go
+++ UserContentServer/pictures.go
@@ -6,9 +6,9 @@ import (
 
 const (
   PORT = 7896
-  DB = "mongodb://localhost:27017"
-  DB_NAME = "Eventorio-dev"
-  ORIG_PATH = "./pictures"
+  DB = "mongodb://localhost:34563"
+  DB_NAME = "eventorio"
+  ORIG_PATH = "/home/admin/pictures"
 )
 func main() {
   var service, err = ContentService.Create(ORIG_PATH, DB, DB_NAME)
