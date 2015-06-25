0ad40aaf25becd74a063cb02a1d6cc6f4b320fb3
mgrin <mr6r1n@gmail.com>
2015-06-25 09:05:32 +0200

    improve gulpfile for the development

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var jsmin = require('gulp-jsmin');
 var stylus = require('gulp-stylus');
 var jade = require('gulp-jade');
+var livereload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
 
 var fs = require('fs-extra');
 
@@ -15,11 +17,13 @@ gulp.task('compile', function () {
 
   gulp.src('./public/stylus/*.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(livereload());
 
   gulp.src('./public/jade/*.jade')
     .pipe(jade({}))
-    .pipe(gulp.dest('./public/view/'));
+    .pipe(gulp.dest('./public/view/'))
+    .pipe(livereload());
 });
 
 gulp.task('compress', function() {
@@ -28,10 +32,12 @@ gulp.task('compress', function() {
   if (ENV === 'production') {
     gulp.src(['./public/js/*.js', './public/js/*/*.js'])
       .pipe(jsmin())
-      .pipe(gulp.dest('./public/cdn/js/'));
+      .pipe(gulp.dest('./public/cdn/js/'))
+      .pipe(livereload());
   } else {
     gulp.src(['./public/js/*.js', './public/js/*/*.js'])
-    .pipe(gulp.dest('./public/cdn/js/'));
+    .pipe(gulp.dest('./public/cdn/js/'))
+    .pipe(livereload());
   }
 });
 
@@ -42,5 +48,17 @@ gulp.task('clean', function () {
 });
 
 gulp.task('default', ['clean', 'compile', 'compress'], function () {
+  if (process.env.NODE_ENV === 'production') return;
 
+  livereload.listen();
+
+  gulp.watch('public/stylus/*styl', ['clean', 'compile']);
+  gulp.watch('public/jade/*.jade', ['clean', 'compile']);
+  gulp.watch('public/js/*.js', ['clean', 'compress']);
+
+  nodemon({
+    script: 'app.js',
+    ignore: 'public/*',
+    tasks: ['clean', 'compile', 'compress']
+  });
 });
